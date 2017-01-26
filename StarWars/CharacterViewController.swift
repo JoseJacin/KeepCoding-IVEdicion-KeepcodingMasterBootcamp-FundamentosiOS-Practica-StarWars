bc90bfda864aa4f0ff15d44fa42486bc12f305a4
José Sánchez <joseynene@hotmail.com>
2017-01-26 23:24:33 +0100

    sonido

--- StarWars/CharacterViewController.swift
+++ StarWars/CharacterViewController.swift
@@ -7,12 +7,16 @@
 //
 
 import UIKit
+import AVFoundation
 
 class CharacterViewController: UIViewController {
 
     //MARK: - Properties
     @IBOutlet weak var photoView: UIImageView!
     var model: StarWarsCharacter
+    // Se crea una variable que sirve para leer y reproducir un fichero de audio. Se tiene que crear como variable de instancia para que la varible dure lo mismo que 
+    // la instancia de CharacterViewController y no dure el tiempo que dura la ejecución de la función en la que se use
+    var player: AVAudioPlayer? = nil
     
     //MARK: - Initialization
     init(model: StarWarsCharacter) {
@@ -42,6 +46,22 @@ class CharacterViewController: UIViewController {
     //MARK: - Actions
     // Acción que reproduce el sonido adjunto del StarWarsCharacter
     @IBAction func playSound(_ sender: UIBarButtonItem) {
+        // Se reproduce el sonido
+        do {
+            // Se avisa que se va a reproducir sonido
+            // Los métodos shared o default indican que se está accediendo a un singleton, por lo que solo habrá una sola instancia
+            try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback)
+            try AVAudioSession.sharedInstance().setActive(true)
+            
+            // Se indica el formato de sonido que se va a reproducir (no es estrictamente necesario, ya que es capaz que coger el formato de la extensión del nombre del fichero
+            player = try AVAudioPlayer(data: model.soundData, fileTypeHint: AVFileTypeCoreAudioFormat)
+            
+            // Se reproduce el sonido
+            player?.play()
+        } catch let err as NSError {
+            print("error reproduciendo sonido")
+            print("\(err)")
+        }
     }
     
     // Acción que abre Wikipedia con la información del StarWarsCharacter
